00db58dedde6567737bdb927531df0cca5e84726
Juan Coutinho <juancoutinho.flooxmongagua@gmail.com>
2025-02-06 09:25:37 -0300

    feat: add display loading for upload (Modal)

--- app/public/src/controller/dropboxController.js
+++ app/public/src/controller/dropboxController.js
@@ -4,6 +4,8 @@ class DropBoxControler {
 
         this.btnSendFIleEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files');
+        this.snackModalEL = document.querySelector('#react-snackbar-root')
+        
         this.initEvents();
     }
 
@@ -11,5 +13,12 @@ class DropBoxControler {
         this.btnSendFIleEl.addEventListener('click', event => {
             this.inputFilesEl.click();
         })
+ 
+     this .inputFilesEl.addEventListener('change', event => {
+        console.log(event.target.files,
+
+            this.snackModalEL.style.display = 'block'
+        )
+     })
     }
 }
